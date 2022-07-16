4cde2fb8b33523783220ed0c5299b6e36fa436b9
“sydasif” <sydasif78@gmail.com>
2022-07-16 11:30:45 +0500

    netmiko script

---
+++ netmiko-docker/app.py
@@ -0,0 +1,21 @@
+# Enable mode sample
+from netmiko import ConnectHandler
+
+# Create a dictionary for a particular device
+S1 = {
+ 'device_type': 'cisco_ios',
+ 'ip': '172.17.0.10',
+ 'username': 'admin',
+ 'password': 'cisco',
+}
+
+'''calling the ConnectHandler Library [**S1] means telling
+python to consider the contents of the dictionary as key value pairs
+instead of single elements.'''
+
+net_connect = ConnectHandler(**S1)
+#net_connect.enable()  # device enable mode
+
+# Sending a command in to the switch --->
+output = net_connect.send_command("show ip int br")
+print(output)
